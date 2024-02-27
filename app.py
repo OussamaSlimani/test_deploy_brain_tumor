37cbe7684d388721dd3ea1e888bdd8d0734b1f37
Oussama Slimani <118611026+OussamaSlimani@users.noreply.github.com>
2024-02-27 23:06:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask_caching import Cache
 import os
 import tensorflow as tf
 import numpy as np
@@ -7,32 +6,16 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-def load_model_if_necessary():
-    # Load the model if it's not already in the cache
-    if not cache.get('model'):
-        # Define the classes list
-        classes = ['Glioma Tumor', 'Meningioma Tumor', 'Normal', 'Pituitary Tumor']
-
-        # Specify the path to the saved model
-        model_path = "Model.h5"
-
-        # Load the trained model
-        model = load_model(model_path)
-        
-        # Cache the model for subsequent requests
-        cache.set('model', model, timeout=None)
-        cache.set('classes', classes, timeout=None)
-
-@cache.memoize(timeout=60)  # Cache the prediction result for 60 seconds
 def predict_tumor_class(image_path):
-    # Load the model if not already in the cache
-    load_model_if_necessary()
+    # Define the classes list
+    classes = ['Glioma Tumor', 'Meningioma Tumor', 'Normal', 'Pituitary Tumor']
 
-    # Retrieve the model from the cache
-    model = cache.get('model')
-    classes = cache.get('classes')
+    # Specify the path to the saved model
+    model_path = "Model.h5"
+
+    # Load the trained model
+    model = load_model(model_path)
 
     # Load and preprocess the image
     img = image.load_img(image_path, target_size=(299, 299))
